99d09ce83080dec883cb696debb8a1449c0d70b9
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2023-06-19 16:20:44 +0700

    2 Preview commit 1: Init/Connected DB/Auth

--- src/main/java/com/example/officebuilding/security/entities/User.java
+++ src/main/java/com/example/officebuilding/security/entities/User.java
@@ -21,6 +21,7 @@ public class User {
     @Column(nullable = false)
     private String fullName;
 
+
     @ManyToMany(fetch = FetchType.EAGER,cascade=CascadeType.ALL)
     @JoinTable(name = "users_roles",
             joinColumns = {@JoinColumn(name = "user_id")},
